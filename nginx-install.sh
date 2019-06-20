cbf39712b8fd386a762419858401e9272286af80
Francisco Filho <francisco@capimgrosso.com>
2019-06-20 15:20:07 -0300

    nginx install final

--- nginx-install.sh
+++ nginx-install.sh
@@ -6,14 +6,12 @@
 #Downloads
 #wget http://luajit.org/download/LuaJIT-2.0.5.tar.gz
 #wget http://nginx.org/download/nginx-1.16.0.tar.gz
-#wget https://github.com/simplresty/ngx_devel_kit/archive/v0.3.0.tar.gz -O ngx_devel_kit.tar.gz
-#wget https://github.com/openresty/lua-nginx-module/archive/v0.10.15.tar.gz -O lua-nginx-module.tar.gz
 
 #LuaJIT
 #make && make install
 
-export LUAJIT_LIB=/usr/local/lib
-export LUAJIT_INC=/usr/local/include/luajit-2.0
+#export LUAJIT_LIB=/usr/local/lib
+#export LUAJIT_INC=/usr/local/include/luajit-2.1
 
 
 #tar -zxvf nginx-1.10.1.tar.gz
@@ -25,10 +23,9 @@ export LUAJIT_INC=/usr/local/include/luajit-2.0
     --pid-path=/usr/local/nginx/nginx.pid \
     --with-stream                         \
     --with-http_ssl_module                \
-    --with-http_v2_module                 \
-    --with-openssl=/dados/Downloads/openssl-1.0.2k \
-    --add-module=/dados/Downloads/ngx_devel_kit-0.3.0  \
-    --add-module=/dados/Downloads/lua-nginx-module-0.10.15
+    --with-http_v2_module
+#    --with-ld-opt="-Wl,-rpath,$LUAJIT_LIB" \
+#    --with-openssl=/dados/Downloads/openssl-1.0.2k
 
 #make
 #sudo make install
